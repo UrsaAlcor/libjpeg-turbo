c596e122a74325a7bfd2df47dc38fa4a22f8bee1
Pierre Delaunay <pierre@delaunay.io>
2022-05-19 13:19:55 -0400

    Replace remaining lz4 references

---
+++ tests/libjpeg_main.c
@@ -0,0 +1,15 @@
+#include <jpeglib.h>
+
+
+int main(int argc, const char* argv[]){
+
+
+    const char* src;
+    const char* dst;
+    int srcSize;
+    int dstCapacity;
+
+    int written = LZ4_compress_default(src, dst, srcSize, dstCapacity);
+
+    return 0;
+}
